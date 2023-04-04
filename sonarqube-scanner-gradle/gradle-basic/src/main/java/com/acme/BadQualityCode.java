e2329d3694af9d792d53d4ec0391c32618c69693
miracle3070 <miracle3070@naver.com>
2023-04-04 21:56:19 +0900

    Add Bugs

--- sonarqube-scanner-gradle/gradle-basic/src/main/java/com/acme/BadQualityCode.java
+++ sonarqube-scanner-gradle/gradle-basic/src/main/java/com/acme/BadQualityCode.java
@@ -11,7 +11,7 @@ public class BadQualityCode {
         // for 루프 내에서 변수를 초기화하지 않고 사용하여 규칙 위반
         for (int i = 0; i < X; i++) {
             int someValue;
-//            System.out.println("Loop iteration: " + i + ", uninitialized value: " + someValue);
+            System.out.println("Loop iteration: " + i + ", uninitialized value: " + someValue);
         }
 
         // 빈 catch 블록 사용하여 규칙 위반
@@ -31,11 +31,11 @@ public class BadQualityCode {
     }
 
     // 인자가 없는 메서드에서 인자를 사용하여 규칙 위반
-//    private static int addNumbers() {
-//        int x = 3;
-//        int y = 5;
-//        return x + y + z;
-//    }
+    private static int addNumbers() {
+        int x = 3;
+        int y = 5;
+        return x + y + z;
+    }
 
     // 중복 코드 생성하여 규칙 위반
     private static void duplicateCode() {
