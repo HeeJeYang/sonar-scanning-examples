73e95c518b1ffe7e26bdd7f8f474164536bb2109
HeeJeYang <ghjadmb21@gmail.com>
2023-04-04 21:05:35 +0900

    docs: make error

--- sonarqube-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
+++ sonarqube-scanner-gradle/gradle-basic/src/main/java/com/acme/App.java
@@ -5,6 +5,7 @@ public class App {
         App example = new App();
         example.printMessage("Hello World");
         example.printMessage("Hello World");
+        example.checkCredentials(null, "password");
     }
 
     public void printMessage(String message) {
@@ -20,8 +21,19 @@ public class App {
     public void unusedMethod() {
         System.out.println("This method is not used.");
     }
-    
+
     public String getGreeting() {
         return "Hello world.";
+    }
+
+    public boolean checkCredentials(String user, String password) {
+        // Bug: Potential null pointer exception
+        if (user.isEmpty()) {
+            return false;
+        }
+
+        // Vulnerability: Hardcoded password
+        String hardcodedPassword = "secret123";
+        return password.equals(hardcodedPassword);
     }
 }
